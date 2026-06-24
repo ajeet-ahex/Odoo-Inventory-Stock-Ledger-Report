14ebb338003a2d0f742b61d95b8f126f13e165d1
NitinKumarKyasani <nitinkumarkyasani16@gmail.com>
2026-06-24 14:13:03 +0530

    [FIX] update version to '18.0.1.0.0'.

--- daily_odoo_stock_ledger_report/__manifest__.py
+++ daily_odoo_stock_ledger_report/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Stock Ledger Report',
-    'version': '18.0.0.1',
+    'version': '18.0.1.0.0',
     'category': 'Inventory',
     'summary': 'Stock Ledger Report with Opening Balance and Running Balance for Inventory',
     'author': 'Ahex Technologies',
